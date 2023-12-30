bdb6dee0fefbb5ad7b3b2a3474104a3e49959ea2
Ghada Al Sarakbi <76967931+ghadasarakbi@users.noreply.github.com>
2023-12-30 23:25:36 +0300

    Update AddDataToDatabase.py

--- AddDataToDatabase.py
+++ AddDataToDatabase.py
@@ -4,7 +4,7 @@ from firebase_admin import db
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://attendencesystem4561-default-rtdb.firebaseio.com/"
+    'databaseURL': ""
 })
 
 
